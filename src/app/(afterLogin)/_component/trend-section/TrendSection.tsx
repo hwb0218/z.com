002d9ce0af946acbbd15cfd6bf045c1eff195c42
FEWoobIn <hwb0218@gmail.com>
2024-04-21 03:35:16 +0900

    feat: trand section 컴포넌트 추가

---
+++ src/app/(afterLogin)/_component/trend-section/TrendSection.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Trend from "./trend/Trend";
+
+import styles from "./trendSection.module.css";
+
+export default function TrendSection() {
+  return (
+    <div className={styles.trendBg}>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </div>
+  );
+}
