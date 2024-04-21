eb44fb83fa5ca7023c7ad6cfd93885ed5bb3d703
FEWoobIn <hwb0218@gmail.com>
2024-04-21 19:31:17 +0900

    style: afterLogin layout

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -7,18 +7,18 @@ import LogoutButton from "./_component/logout-button/LogoutButton";
 import TrendSection from "./_component/trend-section/TrendSection";
 import FollowRecommend from "./_component/follow-recommend/FollowRecommend";
 
-import style from "@/app/(afterLogin)/layout.module.css";
+import styles from "@/app/(afterLogin)/layout.module.css";
 
 import ZLogo from "/public/zlogo.png";
 
 export default function AfterLoginLayout({ children }: PropsWithChildren) {
   return (
-    <div className={style.container}>
-      <header className={style.leftSectionWrapper}>
-        <section className={style.leftSection}>
-          <div className={style.leftSectionFixed}>
-            <Link className={style.logo} href="/home">
-              <div className={style.logoPill}>
+    <div className={styles.container}>
+      <header className={styles.leftSectionWrapper}>
+        <section className={styles.leftSection}>
+          <div className={styles.leftSectionFixed}>
+            <Link className={styles.logo} href="/home">
+              <div className={styles.logoPill}>
                 <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
               </div>
             </Link>
@@ -26,7 +26,7 @@ export default function AfterLoginLayout({ children }: PropsWithChildren) {
               <ul>
                 <NavMenu />
               </ul>
-              <Link href="/compose/tweet" className={style.postButton}>
+              <Link href="/compose/tweet" className={styles.postButton}>
                 게시하기
               </Link>
             </nav>
@@ -34,12 +34,12 @@ export default function AfterLoginLayout({ children }: PropsWithChildren) {
           </div>
         </section>
       </header>
-      <div className={style.rightSectionWrapper}>
-        <div className={style.rightSectionInner}>
-          <main className={style.main}>{children}</main>
-          <section className={style.rightSection}>
+      <div className={styles.rightSectionWrapper}>
+        <div className={styles.rightSectionInner}>
+          <main className={styles.main}>{children}</main>
+          <section className={styles.rightSection}>
             <div style={{ marginBottom: 60, width: "inherit" }}>
-              <form className={style.search}>
+              <form className={styles.search}>
                 <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
                   <g>
                     <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
@@ -49,7 +49,7 @@ export default function AfterLoginLayout({ children }: PropsWithChildren) {
               </form>
             </div>
             <TrendSection />
-            <div className={style.followRecommend}>
+            <div className={styles.followRecommend}>
               <h3>팔로우 추천</h3>
               <FollowRecommend />
               <FollowRecommend />
