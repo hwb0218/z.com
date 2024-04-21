759f4f7951aa230794ff5cd12cab2e2f57e603b1
FEWoobIn <hwb0218@gmail.com>
2024-04-21 19:34:03 +0900

    feat: afterLogin home

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import style from "./home.module.css";
+
+import Tab from "@/app/(afterLogin)/home/_component/tab/Tab";
+import TabProvider from "@/app/(afterLogin)/home/_provider/TabProvider";
+import PostForm from "@/app/(afterLogin)/home/_component/post-form/PostForm";
+import Post from "@/app/(afterLogin)/_component/post/Post";
+
+export default function Page() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
     </main>
-  )
+  );
 }
