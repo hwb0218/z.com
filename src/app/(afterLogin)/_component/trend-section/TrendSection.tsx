145094135912bff6aa25fd9af25a397c331a0a4f
FEWoobIn <hwb0218@gmail.com>
2024-04-28 21:23:56 +0900

    feat: explore 페이지 트랜드섹션 예외처리

--- src/app/(afterLogin)/_component/trend-section/TrendSection.tsx
+++ src/app/(afterLogin)/_component/trend-section/TrendSection.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import React from "react";
 
 import Trend from "./trend/Trend";
 
 import styles from "./trendSection.module.css";
+import { usePathname } from "next/navigation";
 
 export default function TrendSection() {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") {
+    return null;
+  }
+
   return (
     <div className={styles.trendBg}>
       <div className={styles.trend}>
