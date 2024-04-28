2113933dbe84a018c104f8b9824ddf09f7874459
FEWoobIn <hwb0218@gmail.com>
2024-04-28 21:20:17 +0900

    feat: profile 페이지

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,5 +1,39 @@
-import React from "react";
+import BackButton from "../_component/back-button/BackButton";
+import Post from "../_component/post/Post";
+
+import style from "./profile.module.css";
 
 export default function Page() {
-  return <div>page</div>;
+  const user = {
+    id: "zerohch0",
+    nickname: "제로초",
+    image: "/5Udwvqim.jpg"
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
