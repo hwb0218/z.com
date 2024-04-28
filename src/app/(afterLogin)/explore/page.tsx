cd8f8ce890d9c0fd551bbccd1bd3668b7cc12d80
FEWoobIn <hwb0218@gmail.com>
2024-04-28 21:29:37 +0900

    feat: explore 페이지

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <div>page</div>;
+import SearchForm from "@/app/(afterLogin)/_component/search-form/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/trend-section/trend/Trend";
+
+import style from "./explore.module.css";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
