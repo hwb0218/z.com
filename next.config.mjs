656b6a6995ccec6986a8287044a4487b0a926cc8
FEWoobIn <hwb0218@gmail.com>
2024-05-11 21:21:00 +0900

    chore: 이미지 최적화 설정, 외부 이미지 허용 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    imageSizes: [16, 32, 48, 64, 96, 128, 322, 384],
+    deviceSizes: [575, 640, 750, 1080, 1200, 1920],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  },
+  reactStrictMode: true
+};
 
 export default nextConfig;
