74b3cc63138f0f02eac88b1ef29bb6085b43d534
FEWoobIn <hwb0218@gmail.com>
2024-05-17 19:45:53 +0900

    feat: Next 클라이언트 환경 API mocking 컴포넌트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { PropsWithChildren } from "react";
+
+import MSWComponent from "./_component/MSWcomponent";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +16,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
