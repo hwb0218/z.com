33dfe03bb8f8c91ff9ddd581979b3cb0801d6300
FEWoobIn <hwb0218@gmail.com>
2024-05-18 21:21:12 +0900

    feat: beforeLogin signupModal 생성
    - backButton 공유 컴포넌트화

---
+++ src/app/(beforeLogin)/_component/signup-modal/SignupModal.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useActionState } from "react";
+
+import BackButton from "../back-button/BackButton";
+
+import signupAction from "@/app/(beforeLogin)/_lib/signupAction";
+
+import style from "./signup.module.css";
+
+function showErrorMessage(message: string | null) {
+  if (message === "no_id") {
+    return "아이디를 입력하세요.";
+  }
+  if (message === "no_name") {
+    return "닉네임을 입력하세요.";
+  }
+  if (message === "no_password") {
+    return "비밀번호를 입력하세요.";
+  }
+  if (message === "no_image") {
+    return "이미지를 업로드하세요.";
+  }
+  if (message === "user_exists") {
+    return "이미 사용 중인 아이디입니다.";
+  }
+  return "";
+}
+
+export default function SignupModal() {
+  const [state, submitAction, isPending] = useActionState(signupAction, {
+    message: null
+  });
+
+  return (
+    <>
+      <div className={style.modalBackground}>
+        <div className={style.modal}>
+          <div className={style.modalHeader}>
+            <BackButton />
+            <div>계정을 생성하세요.</div>
+          </div>
+          <form action={submitAction}>
+            <div className={style.modalBody}>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="id">
+                  아이디
+                </label>
+                <input id="id" name="id" className={style.input} type="text" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="name">
+                  닉네임
+                </label>
+                <input id="name" name="name" className={style.input} type="text" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="password">
+                  비밀번호
+                </label>
+                <input id="password" name="password" className={style.input} type="password" placeholder="" required />
+              </div>
+              <div className={style.inputDiv}>
+                <label className={style.inputLabel} htmlFor="image">
+                  프로필
+                </label>
+                <input id="image" name="image" className={style.input} type="file" accept="image/*" required />
+              </div>
+            </div>
+            <div className={style.modalFooter}>
+              <button type="submit" disabled={isPending} className={style.actionButton}>
+                가입하기
+              </button>
+              <div className={style.error}>{showErrorMessage(state?.message)}</div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
