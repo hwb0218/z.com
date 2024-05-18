6ef4bce510a3dc64a214176b813bb15e5a465d3d
FEWoobIn <hwb0218@gmail.com>
2024-05-18 21:17:53 +0900

    chore: 오탈자 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
 
-import MSWComponent from "./_component/MSWcomponent";
+import MSWComponent from "./_component/MSWComponent";
 
 import "./globals.css";
 
