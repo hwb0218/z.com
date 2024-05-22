392e05d9ba0d183600021fb5d953bdb0fddce622
FEWoobIn <hwb0218@gmail.com>
2024-05-22 20:14:38 +0900

    chore: 환경 변수 추가

--- globals.d.ts
+++ globals.d.ts
@@ -3,7 +3,10 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_API_MOCKING: string | undefined;
       NEXT_PUBLIC_BASE_URL: string | undefined;
+      NEXT_PUBLIC_URL: string | undefined;
     }
   }
   interface Window {}
 }
+
+export {};
