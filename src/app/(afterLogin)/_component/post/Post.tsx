4c998351f20cfcba555918f97859fb50cc08c05b
FEWoobIn <hwb0218@gmail.com>
2024-06-26 23:26:33 +0900

    fix: 에러 임시 조치

--- src/app/(afterLogin)/_component/post/Post.tsx
+++ src/app/(afterLogin)/_component/post/Post.tsx
@@ -17,11 +17,29 @@ import type { Post } from "@/types/post";
 
 interface Props {
   noImage?: boolean;
-  post: Post;
+  post?: Post;
 }
 
-export default function Post({ noImage, post }: Props) {
-  const target = post;
+export default function Post({ noImage }: Props) {
+  const target = {
+    postId: 1,
+    User: {
+      id: "elonmusk",
+      nickname: "Elon Musk",
+      image: "/yRsRRjGO.jpg"
+    },
+    content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
+    createdAt: new Date(),
+    Images: [] as any[]
+  };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
 
   if (Math.random() > 0.5 && !noImage) {
     target.Images.push({ imageId: 1, link: faker.image.urlLoremFlickr() });
